f141f65b56e5d44d8ad65f88fdcfc1deb4089761
Evan and Alex <manuellae68@gmail.com>
2013-06-27 15:13:53 -0500

    Added part support for drawings, continueing work on Guard class

--- Library.rb
+++ Library.rb
@@ -45,6 +45,7 @@ end
 # vvv Untested vvv #
 
 #Drawings
+
 #Todo: 1. Make Drawings.render
 #      2. Make guard procs for Drawings and its subclasses
 #      3. Make drawing groups
@@ -54,33 +55,42 @@ end
 class Drawings
   def render()
   end
-  class Unitcirle < self
-    @radius = 0
-    @x = 0
-    @y = 0
-    def initialize(x, y, radius)
-      @x = x
-      @y = y
-      @radius = radius
-    end
-  end
-  class Unitsquare < self
-    @side_len = 0
-    @x = 0
-    @y = 0
-    def initialize(x, y, side_len)
-      @x = x
-      @y = y
-      @side_len = side_len
-    end
+ # def size()
+ #   if instance_of?(Unitcircle)
+end
+class Unitcircle < Drawings
+  @radius = 0
+  @x = 0
+  @y = 0
+  def initialize(x, y, radius)
+    @x = x
+    @y = y
+    @radius = radius
   end
 end
+
+class Unitsquare < Drawings
+  @side_len = 0
+  @x = 0
+  @y = 0
+  def initialize(x, y, side_len)
+    @x = x
+    @y = y
+    @side_len = side_len
+  end
+  def get_position
+    return [x, y]
+  end
+end
+
     
-# Guard procedures 
+#Guard procedures
  
 class Guard < Test::Unit::TestCase
-  def test_is_image(Image)
+  def test_image_exists(image)
     assert($gimp_iface.gimp_image_is_valid(@imageID), "ImageID is not valid")
   end
+  def test_is_unit_circle(circle)
+    assert((circle.instance_of?(Unitcircle)), "This is not a unit circle")
+  end
 end
-  
